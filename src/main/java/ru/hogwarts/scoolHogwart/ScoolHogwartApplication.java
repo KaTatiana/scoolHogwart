bd15b2193cacdd57476d8f2614dd96a303800dd2
KaTatiana <badinat@mail.ru>
2022-12-15 07:28:32 +0300

    master

--- src/main/java/ru/hogwarts/scoolHogwart/ScoolHogwartApplication.java
+++ src/main/java/ru/hogwarts/scoolHogwart/ScoolHogwartApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScoolHogwartApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ScoolHogwartApplication.class, args);
 	}
