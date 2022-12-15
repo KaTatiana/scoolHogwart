6285353b019763ce6561278f8f041552c613f076
KaTatiana <badinat@mail.ru>
2022-12-15 07:27:15 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/scoolHogwart/ScoolHogwartApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.scoolHogwart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoolHogwartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoolHogwartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hogwarts/scoolHogwart/ScoolHogwartApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.scoolHogwart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoolHogwartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
